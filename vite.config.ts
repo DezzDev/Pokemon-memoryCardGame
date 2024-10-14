67b6ce78356d186af06c37c00a096ad0dd08a4f6
Daniel Zapata <daniel-dezz@hotmail.com>
2024-10-14 11:22:21 +0200

    change base url to gitHub

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig(({command})=>{
 	if(command === "build"){
 		return {
-			base:"/pokemon-card-game/",
+			base:"/Pokemon-memoryCardGame/",
 			plugins: [react()],
 		};
 	}else{
