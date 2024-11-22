1011265216e5fe3e604db4fac6b08eaceda1b8d1
Daniel Zapata <daniel-dezz@hotmail.com>
2024-11-22 11:49:02 +0100

    apply gradient color to background cards

--- src/components/cardPokemon/CardPokemon.tsx
+++ src/components/cardPokemon/CardPokemon.tsx
@@ -4,137 +4,170 @@ import "./CardPokemon.css";
 const types = [
 	{
 		name: "normal",
-		backgroundColor: "#A8A77A", // Verde oliva suave
-		textColor: "#FFFFFF", // Blanco
+		gradientFrom: "from-[#A8A77A]",
+		gradientTo: "to-[#C5C8A7]",
+		textColor: "text-black", // Blanco
 	},
 	{
 		name: "fighting",
-		backgroundColor: "#C22E28", // Rojo oscuro
-		textColor: "#FFFFFF", // Blanco
+		gradientFrom: "from-[#C22E28]",
+		gradientVia: "via-[#D1423C]",
+		gradientTo: "to-[#D6534B]",
+		textColor: "text-white", // Blanco
 	},
 	{
 		name: "flying",
-		backgroundColor: "#A98FF3", // Púrpura claro
-		textColor: "#FFFFFF", // Blanco
+		gradientFrom: "from-[#A98FF3]",
+		gradientVia: "via-[#B9A1F6]",
+		gradientTo: "to-[#C2A9F9]",
+		textColor: "text-white", // Blanco
 	},
 	{
 		name: "poison",
-		backgroundColor: "#A33EA1", // Violeta
-		textColor: "#FFFFFF", // Blanco
+		gradientFrom: "from-[#A33EA1]",
+		gradientVia: "via-[#B14DB0]",
+		gradientTo: "to-[#C25EC0]",
+		textColor: "text-white", // Blanco
 	},
 	{
 		name: "ground",
-		backgroundColor: "#E2BF65", // Beige arenoso
-		textColor: "#000000", // Negro
+		gradientFrom: "from-[#E2BF65]",
+		gradientVia: "via-[#EAD592]",
+		gradientTo: "to-[#F1E6BA]",
+		textColor: "text-black", // Negro
 	},
 	{
 		name: "rock",
-		backgroundColor: "#B6A136", // Marrón piedra
-		textColor: "#FFFFFF", // Blanco
+		gradientFrom: "from-[#B6A136]",
+		gradientTo: "to-[#D6C360]",
+		textColor: "text-white", // Blanco
 	},
 	{
 		name: "bug",
-		backgroundColor: "#A6B91A", // Verde claro
-		textColor: "#000000", // Negro
+		gradientFrom: "from-[#A6B91A]",
+		gradientTo: "to-[#C7DC3C]",
+		textColor: "text-black", // Negro
 	},
 	{
 		name: "ghost",
-		backgroundColor: "#735797", // Púrpura oscuro
-		textColor: "#FFFFFF", // Blanco
+		gradientFrom: "from-[#735797]",
+		gradientTo: "to-[#8E6DB4]",
+		textColor: "text-white", // Blanco
 	},
 	{
 		name: "steel",
-		backgroundColor: "#B7B7CE", // Gris acero
-		textColor: "#000000", // Negro
+		gradientFrom: "from-[#B7B7CE]",
+		gradientTo: "to-[#D1D1EB]",
+		textColor: "text-black", // Negro
 	},
 	{
 		name: "fire",
-		backgroundColor: "#EE8130", // Naranja
-		textColor: "#000000", // Negro
+		gradientFrom: "from-[#EE8130]",
+		gradientTo: "to-[#F5A051]",
+		textColor: "text-black", // Negro
 	},
 	{
 		name: "water",
-		backgroundColor: "#6390F0", // Azul claro
-		textColor: "#FFFFFF", // Blanco
+		gradientFrom: "from-[#6390F0]",
+		gradientTo: "to-[#87ABF6]",
+		textColor: "text-white", // Blanco
 	},
 	{
 		name: "grass",
-		backgroundColor: "#7AC74C", // Verde
-		textColor: "#000000", // Negro
+		gradientFrom: "from-[#7AC74C]",
+		gradientTo: "to-[#A3DE78]",
+		textColor: "text-black", // Negro
 	},
 	{
 		name: "electric",
-		backgroundColor: "#F7D02C", // Amarillo
-		textColor: "#000000", // Negro
+		gradientFrom: "from-[#F7D02C]",
+		gradientTo: "to-[#FFE66A]",
+		textColor: "text-black", // Negro
 	},
 	{
 		name: "psychic",
-		backgroundColor: "#F95587", // Rosa fuerte
-		textColor: "#FFFFFF", // Blanco
+		gradientFrom: "from-[#F95587]",
+		gradientTo: "to-[#FA789E]",
+		textColor: "text-white", // Blanco
 	},
 	{
 		name: "ice",
-		backgroundColor: "#96D9D6", // Azul hielo
-		textColor: "#000000", // Negro
+		gradientFrom: "from-[#96D9D6]",
+		gradientTo: "to-[#B6ECE9]",
+		textColor: "text-black", // Negro
 	},
 	{
 		name: "dragon",
-		backgroundColor: "#6F35FC", // Morado oscuro
-		textColor: "#FFFFFF", // Blanco
+		gradientFrom: "from-[#6F35FC]",
+		gradientTo: "to-[#945CFF]",
+		textColor: "text-white", // Blanco
 	},
 	{
 		name: "dark",
-		backgroundColor: "#705746", // Marrón oscuro
-		textColor: "#FFFFFF", // Blanco
+		gradientFrom: "from-[#705746]",
+		gradientTo: "to-[#8A685A]",
+		textColor: "text-white", // Blanco
 	},
 	{
 		name: "fairy",
-		backgroundColor: "#D685AD", // Rosa pastel
-		textColor: "#000000", // Negro
+		gradientFrom: "from-[#D685AD]",
+		gradientTo: "to-[#E7AFC9]",
+		textColor: "text-black", // Negro
 	},
 	{
 		name: "stellar",
-		backgroundColor: "#D4AF37", // Oro
-		textColor: "#000000", // Negro
+		gradientFrom: "from-[#D4AF37]",
+		gradientTo: "to-[#F0C961]",
+		textColor: "text-black", // Negro
 	},
 	{
 		name: "unknown",
-		backgroundColor: "#68A090", // Gris neutral
-		textColor: "#FFFFFF", // Blanco
+		gradientFrom: "from-[#68A090]",
+		gradientTo: "to-[#8EB8AA]",
+		textColor: "text-white", // Blanco
 	},
 ];
 
 
+
+
 type props = {
-	pokemon : 			PokemonMin,
-	handleChoice : 	(card: PokemonMin)=> void,
-	flipped: 				boolean | undefined,
-	disabled:				boolean
+	pokemon: PokemonMin,
+	handleChoice: (card: PokemonMin) => void,
+	flipped: boolean | undefined,
+	disabled: boolean
 }
 
 export default function CardPokemon({
-	pokemon, 
-	handleChoice, 
+	pokemon,
+	handleChoice,
 	flipped,
 	disabled
-} : props) {
+}: props) {
 
-	const handleClick = ()=>{
+	const handleClick = () => {
 		// while disabled false, we can call handleChoice
-		if(!disabled){
+		if (!disabled) {
 			handleChoice(pokemon);
 		}
 	};
-
+	
 	const type = types.find(type => type.name === pokemon.type);
+
+	// const gradientFrom = type?.gradientFrom || "bg-slate-800";
+	// const gradientVia = type?.gradientVia || "bg-slate-800";
+	// const gradientTo = type?.gradientTo || "bg-slate-800";
+	// const textColor = type?.textColor || "text-white";
+	
+
 
 	return (
 		<div
 			className='relative cursor-pointer h-[176px] w-[128px] sm:h-[220px] sm:w-[160px] md:h-[275px] md:w-[200px]'
 		>
-			
+
 			<div className={`card-content w-full h-full border-[1px] border-white ${flipped ? "flipped" : ""}`}>
-				<div className={`front top-0 absolute  flex flex-col h-full w-full overflow-hidden ${pokemon.matched ? "flash" : ""} ${type ? `text-[${type.textColor}] bg-[${type.backgroundColor}]` : "text-[green]"}`}>
+				<div className={`front top-0 absolute flex flex-col h-full w-full overflow-hidden bg-gradient-to-t ${type?.gradientFrom} ${type?.gradientVia} ${type?.gradientTo || ""} ${type?.textColor} ${pokemon.matched ? "flash" : ""} `}>
 					<img
 						className="h-full w-full block"
 						src={pokemon.img}
@@ -142,7 +175,7 @@ export default function CardPokemon({
 					/>
 					<p
 						className="m-0 p-1 h-full flex justify-center items-center text-center press-start-2p-regular text-xs md:text-sm"
-					>{pokemon.name}</p>
+					>{pokemon.name.toLocaleUpperCase()}</p>
 				</div>
 				<img
 					src="pokemon_card_backside.webp"
