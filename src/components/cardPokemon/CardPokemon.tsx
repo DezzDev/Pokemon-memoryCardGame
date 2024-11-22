f1442849a1320e0b089f1d9ccfb88458a86f1fc8
Daniel Zapata <daniel-dezz@hotmail.com>
2024-11-22 12:26:49 +0100

    rename img card backside

--- src/components/cardPokemon/CardPokemon.tsx
+++ src/components/cardPokemon/CardPokemon.tsx
@@ -178,7 +178,7 @@ export default function CardPokemon({
 					>{pokemon.name.toLocaleUpperCase()}</p>
 				</div>
 				<img
-					src="pokemon_card_backside.PNG"
+					src="pokemon_card_backside_pixel.webp"
 					alt="card back"
 					className='back top-0 absolute'
 					onClick={handleClick}
